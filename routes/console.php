1bdffde8e89d37191eae52d5347ef004e7b959ea
Gilbert <gilberto.vi.ro@gmail.com>
2023-07-23 19:10:58 -0600

    Creando codigo para crear MVC en routes/console.php

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,42 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('mvc:create {name?} {r?}', function () {
+    $name = $this->argument('name');
+    $r = $this->argument('r');
+    $path_c = base_path()."/app/Http/Controllers/".$name."Controller.php";
+    $path_v = base_path()."/resources/views/".lcfirst($name).".blade.php";
+    if(file_exists($path_c))
+        $this->comment("The ".$name." in MVC already exists");
+    else{
+        try{
+            Artisan::call('make:controller '.$name.'Controller '.$r);
+            Artisan::call('make:model '.$name.'Model '.$r);
+            fopen($path_v,"w+");
+            $this->comment("MVC $name was created successfully");
+        }catch(Exception $e){
+            $this->comment("An error occurred while creating MVC $name");
+        }
+    }
+})->purpose('create MVC');
+
+Artisan::command('mvc:delete {name?}', function () {
+    $name = $this->argument('name');
+    $path_c = base_path()."/app/Http/Controllers/".$name."Controller.php";
+    $path_m = base_path()."/app/Models/".$name."Model.php";
+    $path_v = base_path()."/resources/views/".$name.".blade.php";
+    if(!file_exists($path_c))
+        $this->comment("The ".$name." in MVC does not exist");
+    else{
+        try{
+            unlink($path_c);
+            unlink($path_m);
+            unlink($path_v);
+            $this->comment("The MVC $name was removed successfully");
+        }catch(Exception $e){
+            $this->comment("An error occurred while removing MVC $name");
+        }
+    }
+})->purpose('Delete MVC');
